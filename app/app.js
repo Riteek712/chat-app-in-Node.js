eb4810a50be70a6b7d32efb086d8e417ab24def7
Riteek Rakesh <riteek7.12@gmail.com>
2024-02-24 02:32:46 +0530

    basic websocket server

---
+++ app/app.js
@@ -0,0 +1,24 @@
+const socket = new WebSocket('ws://localhost:3000')
+
+function sendMessage(e){
+    e.preventDefault()
+    const input = document.querySelector('input')
+    if(input.value){
+        socket.send(input.value)
+        input.value =""
+    }
+    input.focus()
+}
+
+document.querySelector('form')
+    .addEventListener('submit', sendMessage)
+
+//listen for messages
+socket.addEventListener("message",({data})=>{
+        const li = document.createElement('li')
+        
+        li.textContent = data
+        console.log(li.textContent)
+        document.querySelector('ul').appendChild(li)
+    }
+)
